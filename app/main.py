92ab3513d456e407c85b68fa4ff76de1bd7471b2
张艺严 <zhangyiyan@zhangyiyandembp.mynetworksettings.com>
2024-09-28 04:58:53 -0400

    fulfill User Login, Payment Search by User id, Payment Add and update payment status

--- app/main.py
+++ app/main.py
@@ -4,6 +4,11 @@ from app import models
 from app.database import engine, get_db
 from app.models import review, user, order, payment, notification  # 导入所有模型
 from app.routers.user_service import user_router  # 导入用户相关的 router
+from app.routers.review_service import review_router  # 导入评论相关的 router
+from app.routers.order_service import order_router  # 导入订单相关的 router
+from app.routers.payment_service import payment_router  # 导入支付相关的 router
+from app.routers.notification_service import notification_router  # 导入通知相关的 router
+
 
 # 创建数据库表
 review.Base.metadata.create_all(bind=engine)
@@ -18,6 +23,11 @@ app = FastAPI()
 
 # 这是一个router的示例
 app.include_router(user_router, prefix="/users", tags=["users"])
+app.include_router(review_router, prefix="/reviews", tags=["reviews"])
+app.include_router(order_router, prefix="/orders", tags=["orders"])
+app.include_router(payment_router, prefix="/payments", tags=["payments"])
+app.include_router(notification_router, prefix="/notifications", tags=["notifications"])
+
 
 @app.get("/")
 def read_root():
--- app/routers/user_service.py
+++ app/routers/user_service.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException,Form
 from sqlalchemy.orm import Session
 from app.models.user import User
 from app.database import get_db
+from passlib.context import CryptContext
 
 user_router = APIRouter()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 @user_router.get("/first_user")
@@ -13,3 +15,24 @@ def get_first_user_id(db: Session = Depends(get_db)):
     if not first_user:
         raise HTTPException(status_code=404, detail="No user found")
     return {"id": first_user.id}
+
+@user_router.get("/{username}")
+def get_user(username: str, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.username == username).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"user_id": user.id, "username": user.username, "email": user.email}
+
+@user_router.post("/login")
+def login(username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.username == username).first()
+    if user is None:
+        # 用户不存在
+        raise HTTPException(status_code=404, detail="User not found")
+        # 先进行明文比较，之后再视情况启用哈希比较 
+    elif (password is None) or(user.password != password):
+        # 密码不匹配
+        raise HTTPException(status_code=401, detail="Incorrect password")
+    else:
+        # 登录成功
+        return {"message": "Login successful", "user_id": user.id, "username": user.username}
